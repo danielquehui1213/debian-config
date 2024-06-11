e751389e5af662cfab6e63c961a5a2f72e751c8b
danielquehui1213 <132399543+danielquehui1213@users.noreply.github.com>
2024-06-11 11:35:21 -0500

    debian-to-kali.sh

--- debian-to-kali.sh
+++ debian-to-kali.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com ED444FF07D8D0BF6 
+
+sudo apt update
+
+clear
+
+echo "hello my friend , wellcome to kali in debian"
+
+neofetch
